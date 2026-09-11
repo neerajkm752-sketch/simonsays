aeb6ad415628ef28f9af87e883dcfc6f87f1bfda
neerajkm752-sketch <neerajkm752@gmail.com>
2026-09-11 08:38:50 +0530

    adding new code

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log("JS FILE CONNECTED");
+let GameSeq=[];
+let userSeq=[];
+
+let started=false;
+let level=0;
+
+document.addEventListener("keydown",function(){
+    if(started==false){
+    console.log("Game started");
+     started=true;
+    }
+   
+})
